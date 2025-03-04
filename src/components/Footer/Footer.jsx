d12072683d7cdf4fcf7eb890ef4835e43058d6fc
Kevin Jimenez <KevinJG994@gmail.com>
2025-03-04 10:52:55 +0000

    feat: Movies, Series and Favourites Pages implements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10 z-20 relative">
-      <aside className="flex flex-col items-center">
+      <aside className="flex flex-col items-center m-auto">
         <figure>
           <img src="/KevFlix-logo.png" alt="KevFlix Logo" title='KevFlix' className='w-20' />
         </figure>
--- src/pages/SeriePage/SeriePage.jsx
+++ src/pages/SeriePage/SeriePage.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+import React from "react";
+import "../../App.css";
+export default function SeriePage() {
+  const movies = [
+    {
+      image:
+        "https://www.lavanguardia.com/peliculas-series/images/movie/poster/2024/3/w1280/6tJWxRfBKWGIPFkfLTod2CgCexU.jpg",
+      title: "Danmel",
+      year: "2024",
+    },
+    {
+      image:
+        "https://lumiere-a.akamaihd.net/v1/images/image_81e2d881.jpeg?region=0%2C0%2C540%2C810&width=320",
+      title: "Capitan America 4",
+      year: "2020",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR96g6yzjwSoAXveuHJQtozPKKWi74tKA2nqA&s",
+      title: "Thurderbolts",
+      year: "2014",
+    },
+    {
+      image:
+        "https://es.web.img3.acsta.net/c_310_420/img/c6/f8/c6f8e0d63437fb7df22483178e2d8f2c.jpg",
+      title: "Red One",
+      year: "2021",
+    },
+    {
+      image:
+        "https://www.lavanguardia.com/peliculas-series/images/movie/poster/2024/3/w1280/6tJWxRfBKWGIPFkfLTod2CgCexU.jpg",
+      title: "Danmel",
+      year: "2024",
+    },
+    {
+      image:
+        "https://lumiere-a.akamaihd.net/v1/images/image_81e2d881.jpeg?region=0%2C0%2C540%2C810&width=320",
+      title: "Capitan America 4",
+      year: "2020",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR96g6yzjwSoAXveuHJQtozPKKWi74tKA2nqA&s",
+      title: "Thurderbolts",
+      year: "2014",
+    },
+    {
+      image:
+        "https://es.web.img3.acsta.net/c_310_420/img/c6/f8/c6f8e0d63437fb7df22483178e2d8f2c.jpg",
+      title: "Red One",
+      year: "2021",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR96g6yzjwSoAXveuHJQtozPKKWi74tKA2nqA&s",
+      title: "Thurderbolts",
+      year: "2014",
+    },
+    {
+      image:
+        "https://es.web.img3.acsta.net/c_310_420/img/c6/f8/c6f8e0d63437fb7df22483178e2d8f2c.jpg",
+      title: "Red One",
+      year: "2021",
+    },
+  ];
 
-export default function SeriePAge() {
   return (
-    <div>SeriePAge</div>
-  )
+    <div className="flex flex-col min-h-screen p-10 mt-20 m-auto">
+      <h2 className="text-3xl mb-6 ml-12 text-center md:text-left">
+        Últimas Series
+      </h2>
+
+      <div className="calc-width">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {movies.map((item, index) => (
+            <div
+              key={index}
+              className="relative w-full min-w-0 h-96 shadow-xl overflow-hidden group"
+            >
+              <figure className="w-full h-full">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="w-full h-full object-cover"
+                />
+              </figure>
+              <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <p className="text-white text-lg mb-2">{item.title}</p>
+                <button className="bg-white text-black px-4 py-2 rounded-lg">
+                  {item.year}
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
