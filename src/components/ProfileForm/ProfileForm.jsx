adde5b28901e32490bb715aebf5edd8ad9a90e67
KevinJG994 <KevinJG994@gmail.com>
2025-03-10 17:31:33 +0000

    feat: CRUD users implements

--- src/components/ProfileForm/ProfileForm.jsx
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -1,26 +1,72 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import userService from '../../services/users.service';
+import { AuthContext } from '../../context/auth.context';
 
 export default function ProfileForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [image, setImage] = useState('');
+  
+    const [errorMessage, setErrorMessage] = useState(undefined);
+  
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+  
+    useEffect(() => {
+      if (user && user._id) {
+        userService
+          .getUserById(user._id)
+          .then((response) => {
+            const profile = response.data;
+            setName(profile.name);
+            setEmail(profile.email);
+            setImage(profile.image);
+          })
+          .catch((error) => {
+            const errorDescription = error.response?.data?.message || "An error occurred";
+            setErrorMessage(errorDescription);
+          });
+      }
+    }, [user]);
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+      const formData = { name, email, image };
+  
+      userService
+        .updateUser(user._id, formData)
+        .then((response) => {
+          console.log('Usuario editado:', response.data);
+          navigate(-1);
+        })
+        .catch((error) => {
+          const errorDescription = error.response?.data?.message || "An error occurred";
+          setErrorMessage(errorDescription);
+        });
+    };
+
     return (
         <div className='flex m-auto items-center'>
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Nombre</span>
                     </label>
-                    <input type="text" placeholder="Nombre" className="input input-bordered" required />
+                    <input type="text" placeholder="Nombre" className="input input-bordered" onChange={(e) => setName(e.target.value)} value={name} required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" placeholder="Email" className="input input-bordered" required />
+                    <input type="email" placeholder="email" className="input input-bordered" onChange={(e) => setEmail(e.target.value)} value={email} disabled/>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Foto de perfil</span>
                     </label>
-                    <input type="file" placeholder="Foto de perfil" className="input input-bordered" required />
+                    <input type="text" placeholder="Foto de perfil" className="input input-bordered" onChange={(e) => setImage(e.target.value)} value={image} required />
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Modificar datos</button>
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -4,12 +4,13 @@ import "../../App.css";
 import { AuthContext } from "../../context/auth.context";
 import ProfileForm from "../../components/ProfileForm/ProfileForm";
 import Loading from "../../components/Loading/Loading";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
   const [profile, setProfile] = useState(null);
   const [errorMessage, setErrorMessage] = useState(undefined);
 
-  const { user } = useContext(AuthContext); // Obtener el usuario autenticado del contexto
+  const { user, logOutUser } = useContext(AuthContext); // Obtener el usuario autenticado del contexto
 
   useEffect(() => {
     if (user && user._id) {
@@ -24,6 +25,18 @@ function ProfilePage() {
         });
     }
   }, [user]);
+
+  const handleDelete = () => {
+    const isConfirmed = window.confirm("¿De verdad quieres eliminar el usuario?")
+
+    if (isConfirmed) {
+      userService
+        .deleteUser(user._id)
+        .then(() => {
+          logOutUser()
+        })
+    };
+  }
 
   return (
     <div className="hero bg-base-100 min-h-screen flex flex-col items-center calc-width-navbar">
@@ -58,9 +71,9 @@ function ProfilePage() {
                   </div>
                 </dialog>
 
-                <button className="btn btn-outline btn-primary" title="Favourite">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                <button className="btn btn-outline btn-primary" title="Delete" onClick={handleDelete}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 hover:text-primary-color">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                   </svg>
                 </button>
               </div>
@@ -76,7 +89,7 @@ function ProfilePage() {
             </div>
           </>
         ) : (
-         <Loading />
+          <Loading />
         )}
       </div>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
