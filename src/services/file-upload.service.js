6e57cda2d5a98dad2b0452a027cdb5bf4c929d56
KevinJG994 <KevinJG994@gmail.com>
2025-03-13 12:13:00 +0000

    fix: environment url to cloudinary

--- src/services/file-upload.service.js
+++ src/services/file-upload.service.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
     // make sure you use PORT = 5005 (the port where our server is running)
-    baseURL: "http://localhost:5005/api"
+    baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
     // withCredentials: true // => you might need this option if using cookies and sessions
 });
+
 
 const errorHandler = (err) => {
     throw err;
